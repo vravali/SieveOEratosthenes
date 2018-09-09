fdb7d9c17b43259f670be2f206ebde36cefc1286
vravali <vangav@brandeis.edu>
2018-09-09 17:10:46 -0400

    First Attempt

---
+++ FirstJenkinsProject/src/edu/vvangala/jenkins/code/SieveOfEratosthenes.java
@@ -0,0 +1,46 @@
+package edu.vvangala.jenkins.code;
+
+import java.util.Scanner;
+
+/**
+ * @author VeenaRavali
+ *
+ */
+public class SieveOfEratosthenes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String A;
+		int N;
+	//	Scanner sc= new Scanner(System.in);
+	//	System.out.print("enter the prime number ceiling: ");
+	//	N = sc.nextInt();
+	// sc.close();
+	A= args[0];
+	//	N = 50;
+	N= Integer.parseInt(A);
+		//
+		boolean[] isPrime = new boolean[N];
+		isPrime[0] = false;
+		for (int c =1; c< N; c++){
+			isPrime[c] = true;
+			
+		}
+//
+		for (int i=2;  i<=N; i++){
+			//
+			if (isPrime[i-1]) {
+				// print out prime number
+				System.out.println(i);
+				
+				for (int j =i*i; j<=N; j += i) {
+					isPrime[j-1] = false;
+				}
+			}
+		}
+	}
+
+
+}
